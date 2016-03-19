4b8ef350bc1bb972caba46a15deda1ce3e8f498b
Muhammed Othman <muhammed.ottman@gmail.com>
2016-03-19 15:58:46 -0700

    adding function implementation to helpers

--- dataStructures/dataStructures/helpers.c
+++ dataStructures/dataStructures/helpers.c
@@ -5,5 +5,25 @@
 //  Created by Muhammed Othman on 3/19/16.
 //  Copyright © 2016 Muhammed Othman. All rights reserved.
 //
-
+#include <stdio.h>
 #include "helpers.h"
+
+void push(int n)
+{
+    // TODO
+}
+
+void pop(void)
+{
+    // TODO
+}
+
+void enqueue(int n)
+{
+    // TODO
+}
+
+void dequeue(void)
+{
+    // TODO
+}
